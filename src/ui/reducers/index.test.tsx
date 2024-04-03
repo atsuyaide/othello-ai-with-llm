4d454e284a669deea884317c6f5f80d3fa2fd5ea
Atsuya Ide <kappa_deatsu@yahoo.co.jp>
2024-04-03 22:10:07 +0900

    add test reducer

--- src/ui/reducers/index.test.tsx
+++ src/ui/reducers/index.test.tsx
@@ -1,9 +1,10 @@
+import * as Action from "@app/ui/actions";
+import { initialBoard } from "@app/ui/constants";
+import { GameState } from "@app/ui/containers/Game";
+import { move, reducers } from "@app/ui/reducers";
+import { CellState } from "@app/ui/types";
 import * as _ from "lodash";
 import { describe, expect, it } from "vitest";
-import { initialBoard } from "../constants";
-import { GameState } from "../containers/Game";
-import { CellState } from "../types";
-import { move } from "./index";
 
 describe("move", () => {
   it("should return the updated game state when a valid move is made", () => {
@@ -107,6 +108,215 @@ describe("move", () => {
     };
 
     const newState = move(initialState);
+
+    expect(newState).toEqual(expectedState);
+  });
+});
+
+describe("reducers", () => {
+  it("should handle click_cell action when it is the player's turn", () => {
+    const initialState: GameState = {
+      positions: [
+        {
+          cells: initialBoard,
+          turn: "b",
+        },
+      ],
+      latestMove: undefined,
+      playerColor: "b",
+    };
+    const action = Action.clickCell({ x: 5, y: 4 });
+
+    const expectedBoard: CellState[] = _.flatten(
+      [
+        "........",
+        "........",
+        "........",
+        "...wb...",
+        "...bbb..",
+        "........",
+        "........",
+        "........",
+      ].map((r) => r.split("") as CellState[])
+    );
+    const expectedState = {
+      positions: [
+        {
+          cells: initialBoard,
+          turn: "b",
+        },
+        {
+          cells: expectedBoard,
+          turn: "w",
+        },
+      ],
+      latestMove: { x: 5, y: 4 },
+      playerColor: "b",
+    };
+    const newState = reducers(initialState, action);
+
+    expect(newState).toEqual(expectedState);
+  });
+
+  it("should handle click_pass action when it is the player's turn", () => {
+    const canNotMoveBoard: CellState[] = _.flatten(
+      [
+        "........",
+        "........",
+        "........",
+        "...ww...",
+        "...ww...",
+        "........",
+        "........",
+        "........",
+      ].map((r) => r.split("") as CellState[])
+    );
+    const initialState: GameState = {
+      positions: [
+        {
+          cells: canNotMoveBoard,
+          turn: "b",
+        },
+      ],
+      latestMove: undefined,
+      playerColor: "b",
+    };
+    const action = Action.clickPass;
+
+    const expectedState: GameState = {
+      positions: [
+        {
+          cells: canNotMoveBoard,
+          turn: "b",
+        },
+        {
+          cells: canNotMoveBoard,
+          turn: "w",
+        },
+      ],
+      latestMove: undefined,
+      playerColor: "b",
+    };
+
+    const newState = reducers(initialState, action);
+
+    expect(newState).toEqual(expectedState);
+  });
+
+  it("should handle launch_ai action when it is not the player's turn", () => {
+    const mockBoard: CellState[] = _.flatten(
+      [
+        "........",
+        "........",
+        "........",
+        "........",
+        "...wb...",
+        "........",
+        "........",
+        "........",
+      ].map((r) => r.split("") as CellState[])
+    );
+    const initialState: GameState = {
+      positions: [
+        {
+          cells: mockBoard,
+          turn: "w",
+        },
+      ],
+      latestMove: undefined,
+      playerColor: "b",
+    };
+    const action = Action.aiMove;
+
+    const expectedBoard: CellState[] = _.flatten(
+      [
+        "........",
+        "........",
+        "........",
+        "........",
+        "...www..",
+        "........",
+        "........",
+        "........",
+      ].map((r) => r.split("") as CellState[])
+    );
+    const expectedState = {
+      positions: [
+        {
+          cells: mockBoard,
+          turn: "w",
+        },
+        {
+          cells: expectedBoard,
+          turn: "b",
+        },
+      ],
+      latestMove: { x: 5, y: 4 },
+      playerColor: "b",
+    };
+
+    const newState = reducers(initialState, action);
+
+    expect(newState).toEqual(expectedState);
+  });
+
+  it("should handle click_prev action", () => {
+    const secondBoard: CellState[] = _.flatten(
+      [
+        "........",
+        "........",
+        "........",
+        "...wb...",
+        "...bbb..",
+        "........",
+        "........",
+        "........",
+      ].map((r) => r.split("") as CellState[])
+    );
+    const thirdBoard: CellState[] = _.flatten(
+      [
+        "........",
+        "........",
+        "........",
+        "...wb...",
+        "...bbb..",
+        "........",
+        "........",
+        "........",
+      ].map((r) => r.split("") as CellState[])
+    );
+    const initialState: GameState = {
+      positions: [
+        {
+          cells: initialBoard,
+          turn: "b",
+        },
+        {
+          cells: secondBoard,
+          turn: "w",
+        },
+        {
+          cells: thirdBoard,
+          turn: "b",
+        },
+      ],
+      latestMove: { x: 5, y: 4 },
+      playerColor: "b",
+    };
+    const action = Action.clickPrev;
+
+    const expectedState: GameState = {
+      positions: [
+        {
+          cells: initialBoard,
+          turn: "b",
+        },
+      ],
+      latestMove: undefined,
+      playerColor: "b",
+    };
+
+    const newState = reducers(initialState, action);
 
     expect(newState).toEqual(expectedState);
   });
